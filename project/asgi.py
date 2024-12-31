8878405f57b099f27960d123b2523dbb11d03898
Pushpendra Tripathi <pushpendratripathi@outlook.com>
2024-12-31 16:27:41 +0530

    feat: update the requirements/py31_dj5.txt

--- project/asgi.py
+++ project/asgi.py
@@ -5,12 +5,13 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
+from apps.chat.routing import websocket_urlpatterns
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
-from apps.chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
